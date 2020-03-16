82bd5af95110e943db5c7f736a0be60d2ba0ad18
Erisson Silverio Nu�ez <erisson_silverio@ucne.edu.do>
2020-03-16 16:56:42 -0400

    Creando Proyecto

---
+++ Ap1-SegundoP-ErissonSilverio/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ap1_SegundoP_ErissonSilverio.DAL
+{
+    public class Contexto : DbContext
+    {
+        public int MyProperty { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite(@"Data Source = Anonimus.db");
+        }
+    }
+}
---
+++ Ap1-SegundoP-ErissonSilverio/Entidades/Anonimus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ap1_SegundoP_ErissonSilverio.Entidades
+{
+    public class Anonimus
+    {
+
+
+    }
+}
